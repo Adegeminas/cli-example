2aebaa3466eb3047beca250e4c2742cb85cb8e43
Adegeminas <kokarev.egor@gmail.com>
2020-02-07 15:49:18 +0300

    test

---
+++ code/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const getCurrentDirectoryBase = () => {
+  return path.basename(process.cwd());
+},
+
+const directoryExists = (filePath) => {
+  return fs.existsSync(filePath);
+}
