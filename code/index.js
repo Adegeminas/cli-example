9c9f1ec35c5042f544b30f67467a1aee40d1d724
Adegeminas <kokarev.egor@gmail.com>
2020-02-07 18:22:03 +0300

    ...

--- code/index.js
+++ code/index.js
@@ -1,10 +1,39 @@
-const fs = require('fs');
-const path = require('path');
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const inquirer = require('inquirer');
+const Configstore = require('configstore');
+const CLI = require('clui');
+const Spinner = CLI.Spinner;
 
-const getCurrentDirectoryBase = () => {
-  return path.basename(process.cwd());
-},
+const askQuestions = () => { // return promise
+  const questions = [
+    {
+      name: 'next',
+      type: 'input',
+      message: 'What is your next step?',
+      validate: function(value) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'Please enter your next step.';
+        }
+      },
+    },
+  ];
+  return inquirer.prompt(questions);
+};
 
-const directoryExists = (filePath) => {
-  return fs.existsSync(filePath);
-}
+const conf = new Configstore('ginit');
+conf.set('state', {
+  hp: 100,
+});
+
+
+const f = () => {
+  return false;
+};
+
+conf.set('function', f.toString());
+
+console.log(new Function('return ' + conf.get('function'))()());
